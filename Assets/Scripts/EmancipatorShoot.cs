40b4f7a89e25a6ba5d9d4af13f4faf9d28327f2a
Aescosaurus <michaelbuslik@hotmail.com>
2019-11-01 18:11:48 -0600

    Emancipator start.

--- Assets/Scripts/EmancipatorShoot.cs
+++ Assets/Scripts/EmancipatorShoot.cs
@@ -1,9 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class EmancipatorShoot
 	:
 	MonoBehaviour
 {
+	void Start()
+	{
+		body = GetComponent<Rigidbody2D>();
+		Assert.IsNotNull( body );
+
+		cam = Camera.main;
+		Assert.IsNotNull( cam );
+	}
+
+	void Update()
+	{
+		if( Input.GetAxis( "Attack" ) > 0.0f )
+		{
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint(
+				Input.mousePosition );
+			Vector2 diff = mousePos - ( Vector2 )transform.position;
+			body.AddForce( -diff.normalized * pushForce,
+				ForceMode2D.Impulse );
+
+			// TODO: Spawn bullet.
+		}
+	}
+
+	Rigidbody2D body;
+	Camera cam;
+
+	[SerializeField]
+	float pushForce = 0.0f;
 }
