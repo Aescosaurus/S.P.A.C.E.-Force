39b11a33b7bfbd3c9959f16c885e0d0ab94d0a71
Aescosaurus <michaelbuslik@hotmail.com>
2019-11-02 11:46:05 -0600

    Spawn a ring of asteroids surrounding the play area.

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid
+	:
+	MonoBehaviour
+{
+	void OnCollisionEnter2D( Collision2D other )
+	{
+		print( other.gameObject.tag );
+		if( other.gameObject.tag == "Asteroid" )
+		{
+			Destroy( gameObject );
+		}
+	}
+}
