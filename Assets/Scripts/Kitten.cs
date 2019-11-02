42a493f28129e43afc34bc5e96b6f27359fb964d
Aescosaurus <michaelbuslik@hotmail.com>
2019-11-02 17:57:12 -0600

    Space kitten implementation.

---
+++ Assets/Scripts/Kitten.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Kitten
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		body = GetComponent<Rigidbody2D>();
+		Assert.IsNotNull( body );
+	}
+
+	void OnCollisionEnter2D( Collision2D coll )
+	{
+		if( coll.gameObject.tag == "Player" )
+		{
+			player = coll.gameObject;
+		}
+		else if( coll.gameObject.tag == "SpaceFox" )
+		{
+			// Ouch!
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if( player != null )
+		{
+			Vector2 diff = player.transform.position -
+				transform.position;
+
+			body.AddForce( diff * moveSpeed * Time.deltaTime );
+		}
+	}
+
+	GameObject player = null;
+	Rigidbody2D body;
+
+	const float moveSpeed = 55.2f;
+}
---
+++ Assets/Scripts/KittenSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class KittenSpawner
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		kittenPrefab = Resources.Load<GameObject>(
+			"Prefabs/Kitten" );
+		Assert.IsNotNull( kittenPrefab );
+	}
+
+	void Update()
+	{
+		if( kittenSpawnTimer.Update( Time.deltaTime ) )
+		{
+			kittenSpawnTimer.Reset();
+
+			if( Random.Range( 0.0f,100.0f ) < kittenSpawnRate )
+			{
+				// TODO: Create kitten.
+			}
+		}
+	}
+
+	GameObject kittenPrefab;
+
+	[Tooltip( "Chance of spawning a kitten every second." )]
+	[Range( 0.0f,100.0f )]
+	[SerializeField] float kittenSpawnRate = 0.0f;
+
+	Timer kittenSpawnTimer = new Timer( 1.0f );
+}
