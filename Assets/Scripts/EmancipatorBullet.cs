254df881ee3784552bcc94e37422d5a8092b04ec
Aescosaurus <michaelbuslik@hotmail.com>
2019-11-02 09:43:31 -0600

    Emancipator bullets rotate and despawn after a set time.

---
+++ Assets/Scripts/EmancipatorBullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmancipatorBullet
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		rotationSpeed = Random.Range( rotationSpeedMin,
+			rotationSpeedMax );
+	}
+
+	void Update()
+	{
+		transform.Rotate( 0.0f,0.0f,
+			rotationSpeed * Time.deltaTime );
+
+		if( despawn.Update( Time.deltaTime ) )
+		{
+			Destroy( gameObject );
+		}
+	}
+
+	[SerializeField] Timer despawn = new Timer( 3.5f );
+	[SerializeField] float rotationSpeedMin = 0.0f;
+	[SerializeField] float rotationSpeedMax = 0.0f;
+
+	float rotationSpeed;
+}
