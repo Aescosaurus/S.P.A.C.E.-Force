f7093029449c8780016ed2212a9fa379dc8c82d0
Aescosaurus <michaelbuslik@hotmail.com>
2019-11-02 18:19:02 -0600

    Kitten homeworld now accepts refugee kittens.

--- Assets/Scripts/KittenHomeworld.cs
+++ Assets/Scripts/KittenHomeworld.cs
@@ -11,5 +11,14 @@ public class KittenHomeworld
 		transform.Rotate( 0.0f,0.0f,rotSpeed * Time.deltaTime );
 	}
 
+	void OnTriggerEnter2D( Collider2D coll )
+	{
+		if( coll.tag == "SpaceKitten" )
+		{
+			Destroy( coll.gameObject );
+			// TODO: Give us points!
+		}
+	}
+
 	[SerializeField] float rotSpeed = 0.0f;
 }
