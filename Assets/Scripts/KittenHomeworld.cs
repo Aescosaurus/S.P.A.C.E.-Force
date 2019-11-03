a6dfa0a2a3fcadbda6f9751217f483e8822d06a5
Aescosaurus <michaelbuslik@hotmail.com>
2019-11-02 18:12:45 -0600

    Semi-functional kitten homeworld.

---
+++ Assets/Scripts/KittenHomeworld.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KittenHomeworld
+	:
+	MonoBehaviour
+{
+	void Update()
+	{
+		transform.Rotate( 0.0f,0.0f,rotSpeed * Time.deltaTime );
+	}
+
+	[SerializeField] float rotSpeed = 0.0f;
+}
