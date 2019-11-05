5cbb3493a00a09c2a8cf1220761340a0a049330a
Aescosaurus <michaelbuslik@hotmail.com>
2019-11-04 19:40:30 -0700

    Sounds for when things explode.

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -8,15 +8,31 @@ public class HealthBar
 	:
 	MonoBehaviour
 {
+	void Start()
+	{
+		Assert.IsNotNull( explodeSound );
+
+		explosionPrefab = Resources.Load<GameObject>(
+			"Prefabs/Explosion" );
+		Assert.IsNotNull( explosionPrefab );
+	}
+
 	public void Hurt( int damage )
 	{
 		health -= damage;
 		if( health <= 0 )
 		{
 			// Play explosion animation.
+			var explosion = Instantiate( explosionPrefab );
+			var clip = explosion.GetComponent<AudioSource>();
+			clip.clip = explodeSound;
+			clip.Play();
 			Destroy( gameObject );
 		}
 	}
 
+	GameObject explosionPrefab;
+
 	[SerializeField] int health = 0;
+	[SerializeField] AudioClip explodeSound = null;
 }
