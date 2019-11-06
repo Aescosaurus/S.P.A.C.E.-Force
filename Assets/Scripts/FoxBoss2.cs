d95a757d2408564d6a94aa1aeba560dfa1303224
Aescosaurus <michaelbuslik@hotmail.com>
2019-11-06 10:09:04 -0700

    Level 2 boss start.

---
+++ Assets/Scripts/FoxBoss2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxBoss2
+	:
+	MonoBehaviour
+{
+	void OnDestroy()
+	{
+		LevelHandler.SaveKitty();
+	}
+}
