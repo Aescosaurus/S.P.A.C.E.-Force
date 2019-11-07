1aef80fbfb03c4612a64fd0f3ec47b1ee2950a99
Aescosaurus <michaelbuslik@hotmail.com>
2019-11-06 21:02:37 -0700

    Camera zooms out based on movement speed.

---
+++ Assets/Scripts/CameraFocus.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CameraFocus
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag(
+			"Player" );
+		Assert.IsNotNull( player );
+		body = player.GetComponent<Rigidbody2D>();
+		Assert.IsNotNull( body );
+		cam = GetComponent<Camera>();
+		Assert.IsNotNull( cam );
+
+		startSize = cam.orthographicSize;
+	}
+
+	void Update()
+	{
+		var size = cam.orthographicSize;
+		size = startSize + body.velocity.magnitude /
+			scaleFactor;
+		cam.orthographicSize = size;
+	}
+
+	GameObject player;
+	Rigidbody2D body;
+	Camera cam;
+
+	float startSize;
+	[SerializeField] float scaleFactor = 4.0f;
+}
